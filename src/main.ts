20660ede0f15448f9d70a43dca0d3e6f068bedc6
Baudoux, Loïc <loic.baudoux@solidaris.be>
2026-06-09 10:21:08 +0200

    chore: update Angular and related dependencies to version 20.x
    - Downgraded Angular packages from version 21.2.0 to 20.3.0
    - Updated PrimeNG from version 21.1.2 to 20.4.0
    - Adjusted devDependencies for Angular CLI and related tools to version 20.3.27
    - Modified main.ts to remove zone change detection provider

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowser()
-.bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })] });
+platformBrowser().bootstrapModule(AppModule);
